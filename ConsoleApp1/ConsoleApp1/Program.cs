dc34aab3b0510057b49d4da7838988781d01e624
belal <belal.a75@gmail.com>
2018-11-13 10:24:03 +0200

    Added BubbleSort

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,47 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            int[] arr = new int[] { 6, 7, 5, 8, 2, 1 };
+            PrintArr(arr);
+            BubbleSort(arr);
+            PrintArr2(arr);
+        }
+
+        static void PrintArr(int[] arr)
+        {
+            Console.Write("Array: ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + ",");
+            }
+            Console.WriteLine("\b\b");
+        }
+        static void PrintArr2(int[] arr)
+        {
+            Console.Write("Sorted Array: ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + ",");
+            }
+            Console.WriteLine("\b ");
+        }
+        
+        static int[] BubbleSort(int[] arr)
+        {
+            int temp = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        temp = arr[j + 1];
+                        arr[j + 1] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            return arr;
         }
     }
 }
